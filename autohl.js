8ed2b9646fb20662ade2197a65c77db1e7956622
Brian Kessler <brian.m.kessler@gmail.com>
2014-09-10 21:36:29 -0700

    added the public Porter stemmer code and a skeleton for some of the other functions along with an outline

--- autohl.js
+++ autohl.js
@@ -1,3 +1,41 @@
+/*********
+
+1. Get text from the DOM, may need to deal with formatting tags
+2. Tokenize text into sentences  (split on .!? is first attempt)
+3. Tokenize sentences into words (split on white space is first attempt) 
+4. Remove stop words (not sure if this is needed, but can use Porter's list)
+5. Normalize words; lower-case, punctuation removed, stemmed  (Porter stemmer included)
+6. Compute simliarity matrix (DONE)
+7. Calculate ranks via PageRank (DONE)
+8. Tag words back in the DOM by their rank
+
+**********/
+
+function sentence_tokenizer(raw_text) {
+  /***********
+  
+  Split on [.!?][ ]+ unless abbreviation (single letter U.S.A. e.g. i.e. or known list Mr. Mrs. Dr.)
+  also could handle edge case of quoted character "?" or parenthetical (!!!)
+  
+  ***********/
+}
+
+function word_tokenizer(sentence_text) {
+  /***********
+  
+  Split on whitespace, can't think of any obvious cases to catch
+  
+  ***********/
+}
+
+function is_stop_word(word) {
+  /***********
+  
+  Check if in stop word list
+  
+  ***********/
+}
+
 function sim_text_rank(sent_1,sent_2,options) {
     /****
     Similarity between two sets of words given by the number of words in common
@@ -116,6 +154,199 @@ function check_convergence(scores,prev_scores,tolerance) {
   }
   return true;
 }
+
+// Porter stemmer in Javascript. Few comments, but it's easy to follow against the rules in the original
+// paper, in
+//
+//  Porter, 1980, An algorithm for suffix stripping, Program, Vol. 14,
+//  no. 3, pp 130-137,
+//
+// see also http://www.tartarus.org/~martin/PorterStemmer
+
+// Release 1 be 'andargor', Jul 2004
+// Release 2 (substantially revised) by Christopher McKenzie, Aug 2009
+
+var stemmer = (function(){
+    var step2list = {
+            "ational" : "ate",
+            "tional" : "tion",
+            "enci" : "ence",
+            "anci" : "ance",
+            "izer" : "ize",
+            "bli" : "ble",
+            "alli" : "al",
+            "entli" : "ent",
+            "eli" : "e",
+            "ousli" : "ous",
+            "ization" : "ize",
+            "ation" : "ate",
+            "ator" : "ate",
+            "alism" : "al",
+            "iveness" : "ive",
+            "fulness" : "ful",
+            "ousness" : "ous",
+            "aliti" : "al",
+            "iviti" : "ive",
+            "biliti" : "ble",
+            "logi" : "log"
+        },
+
+        step3list = {
+            "icate" : "ic",
+            "ative" : "",
+            "alize" : "al",
+            "iciti" : "ic",
+            "ical" : "ic",
+            "ful" : "",
+            "ness" : ""
+        },
+
+        c = "[^aeiou]",          // consonant
+        v = "[aeiouy]",          // vowel
+        C = c + "[^aeiouy]*",    // consonant sequence
+        V = v + "[aeiou]*",      // vowel sequence
+
+        mgr0 = "^(" + C + ")?" + V + C,               // [C]VC... is m>0
+        meq1 = "^(" + C + ")?" + V + C + "(" + V + ")?$",  // [C]VC[V] is m=1
+        mgr1 = "^(" + C + ")?" + V + C + V + C,       // [C]VCVC... is m>1
+        s_v = "^(" + C + ")?" + v;                   // vowel in stem
+
+    return function (w) {
+        var     stem,
+            suffix,
+            firstch,
+            re,
+            re2,
+            re3,
+            re4,
+            origword = w;
+
+        if (w.length < 3) { return w; }
+
+        firstch = w.substr(0,1);
+        if (firstch == "y") {
+            w = firstch.toUpperCase() + w.substr(1);
+        }
+
+        // Step 1a
+        re = /^(.+?)(ss|i)es$/;
+        re2 = /^(.+?)([^s])s$/;
+
+        if (re.test(w)) { w = w.replace(re,"$1$2"); }
+        else if (re2.test(w)) { w = w.replace(re2,"$1$2"); }
+
+        // Step 1b
+        re = /^(.+?)eed$/;
+        re2 = /^(.+?)(ed|ing)$/;
+        if (re.test(w)) {
+            var fp = re.exec(w);
+            re = new RegExp(mgr0);
+            if (re.test(fp[1])) {
+                re = /.$/;
+                w = w.replace(re,"");
+            }
+        } else if (re2.test(w)) {
+            var fp = re2.exec(w);
+            stem = fp[1];
+            re2 = new RegExp(s_v);
+            if (re2.test(stem)) {
+                w = stem;
+                re2 = /(at|bl|iz)$/;
+                re3 = new RegExp("([^aeiouylsz])\\1$");
+                re4 = new RegExp("^" + C + v + "[^aeiouwxy]$");
+                if (re2.test(w)) {  w = w + "e"; }
+                else if (re3.test(w)) { re = /.$/; w = w.replace(re,""); }
+                else if (re4.test(w)) { w = w + "e"; }
+            }
+        }
+
+        // Step 1c
+        re = /^(.+?)y$/;
+        if (re.test(w)) {
+            var fp = re.exec(w);
+            stem = fp[1];
+            re = new RegExp(s_v);
+            if (re.test(stem)) { w = stem + "i"; }
+        }
+
+        // Step 2
+        re = /^(.+?)(ational|tional|enci|anci|izer|bli|alli|entli|eli|ousli|ization|ation|ator|alism|iveness|fulness|ousness|aliti|iviti|biliti|logi)$/;
+        if (re.test(w)) {
+            var fp = re.exec(w);
+            stem = fp[1];
+            suffix = fp[2];
+            re = new RegExp(mgr0);
+            if (re.test(stem)) {
+                w = stem + step2list[suffix];
+            }
+        }
+
+        // Step 3
+        re = /^(.+?)(icate|ative|alize|iciti|ical|ful|ness)$/;
+        if (re.test(w)) {
+            var fp = re.exec(w);
+            stem = fp[1];
+            suffix = fp[2];
+            re = new RegExp(mgr0);
+            if (re.test(stem)) {
+                w = stem + step3list[suffix];
+            }
+        }
+
+        // Step 4
+        re = /^(.+?)(al|ance|ence|er|ic|able|ible|ant|ement|ment|ent|ou|ism|ate|iti|ous|ive|ize)$/;
+        re2 = /^(.+?)(s|t)(ion)$/;
+        if (re.test(w)) {
+            var fp = re.exec(w);
+            stem = fp[1];
+            re = new RegExp(mgr1);
+            if (re.test(stem)) {
+                w = stem;
+            }
+        } else if (re2.test(w)) {
+            var fp = re2.exec(w);
+            stem = fp[1] + fp[2];
+            re2 = new RegExp(mgr1);
+            if (re2.test(stem)) {
+                w = stem;
+            }
+        }
+
+        // Step 5
+        re = /^(.+?)e$/;
+        if (re.test(w)) {
+            var fp = re.exec(w);
+            stem = fp[1];
+            re = new RegExp(mgr1);
+            re2 = new RegExp(meq1);
+            re3 = new RegExp("^" + C + v + "[^aeiouwxy]$");
+            if (re.test(stem) || (re2.test(stem) && !(re3.test(stem)))) {
+                w = stem;
+            }
+        }
+
+        re = /ll$/;
+        re2 = new RegExp(mgr1);
+        if (re.test(w) && re2.test(w)) {
+            re = /.$/;
+            w = w.replace(re,"");
+        }
+
+        // and turn initial Y back to y
+
+        if (firstch == "y") {
+            w = firstch.toLowerCase() + w.substr(1);
+        }
+
+        return w;
+    }
+})();
+
+/**************
+
+Test code below
+
+**************/
 
 var stem_sentences = [{'subset':1, 'sensori':1, 'detect':1, 'identifi':1, 'process':1, 'pattern':1, 'signal':1, 'repres':1, 'cluster':1, 'exampl':1, 'import':1}, {'randomli':1, 'subset':1, 'work':1, 'point':1, 'solut':1, 'refin':1, 'choic':1, 'initi':1, 'exemplar':1, 'iter':1, 'choos':1, 'close':1, 'found':1}, {'propag':1, 'measur':1, 'similar':1, 'point':1, 'affin':1, 'call':1, 'take':1, 'which':1, 'between':1, 'pair':1, 'input':1, 'devis':1, 'method':1}, {'gradual':1, 'point':1, 'exchang':1, 'qualiti':1, 'correspond':1, 'messag':1, 'cluster':1, 'emerg':1, 'exemplar':1, 'between':1, 'until':1, 'val':1}, {'propag':1, 'repres':1, 'detect':1, 'identifi':1, 'manuscript':1, 'microarray':1, 'airlin':1, 'sentenc':1, 'imag':1, 'face':1, 'access':1, 'cluster':1, 'citi':1, 'effici':1, 'gene':1, 'affin':1, 'travel':1}, {'propag':1, 'lower':1, 'hundredth':1, 'method':1, 'cluster':1, 'amount':1, 'other':1, 'error':1, 'found':1, 'affin':1}];
 
